3a6614daf13e58a9f24934d028e1873a707c086e
karel.decoene <karel.decoene@hotmail.com>
2018-05-18 07:48:49 -0400

    start van mijn project

---
+++ be/BE/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$products = array(
+  array("titel" => "Bert", "image" => "bert.png", "text" => "Dit is het BERT product", "prijs" => "25,00 EUR"),
+  array("titel" => "Ernie", "image" => "ernie.png", "text" => "Dit is ERNIE", "prijs" => "250,00 EUR"),
+  array("titel" => "Bernie", "image" => "bernie.jpg", "text" => "Dit is het BERNIE SANDERS", "prijs" => "500,00 EUR"),
+  array("titel" => "Rick", "image" => "rickandmorty.jpg", "text" => "Dit is RICK", "prijs" => "750,00 EUR")
+);
+
+?> 
---
+++ examenopdracht/home.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="menu.css">
+  <link rel="stylesheet" type="text/css" href="mainlayout.css">
+  <link rel="stylesheet" type="text/css" href="footer.css">
+  <link rel="stylesheet" type="text/css" href="bestPictures.css">
+  <link rel="stylesheet" type="text/css" href="Carspotter.css">
+</head>
+<body>
+  <div id="gridcontainer">
+    <div id="menu">
+      <ul>
+        <li id="menuStuk1"><a href="#home" class="menu-button"><b>SFE  </b>Supercars</a></li>
+        <li class="menuStuk2"><a href="#pictures" class="menu-button">Pictures</a></li>
+        <li class="menuStuk2"><a href="#future" class="menu-button">Future</a></li>
+        <li class="menuStuk2"><a href="#shop" class="menu-button">Shop</a></li>
+      </ul>
+    </div>
+    
+    <div id="banner">
+      <header class="bigpicture" style="max-width:1500px;" id="homepicture">
+        <img id="homeimage" src="oefeningenPHP/be/imageseindwerk/20171008_091930 (2).jpg">
+         <div class="textonpic">
+            <h1><span id="titlepiece1"><b>SFE</b></span> <span id="titlepiece2">SUPERCARSFROMEVERYWHERE</span></h1>
+         </div>
+      </header>
+    </div>
+    
+    <div id="bestPictures">
+      <h1>
+        Best Pictures
+      </h1>
+      
+      <hr>
+      
+      <div id="Huracan Performante">
+          <img src="/w3images/house5.jpg">
+      </div>
+      <div id="918 Spyder">
+          <img src="/w3images/house5.jpg">
+      </div>
+      <div id="458 Speciale">
+          <img src="/w3images/house5.jpg">
+      </div>
+      <div id="F12 TDF">
+          <img src="/w3images/house5.jpg">
+      </div>
+      <div id="McLaren Senna">
+          <img src="/w3images/house5.jpg">
+      </div>
+      <div id="Koenigsegg Agera RS1">
+          <img src="/w3images/house5.jpg">
+      </div>
+      <div id="Aventador SV">
+          <img src="/w3images/house5.jpg">
+      </div>
+      <div id="AMG GT R Coupé">
+          <img src="/w3images/house5.jpg">
+      </div>
+    </div>
+    
+    <div id="Carspotter">
+      <h1>
+        Carspotting
+      </h1>
+      <hr>
+      <p>
+        Hierboven kon je al enkele foto’s bewonderen maar het begrip carspotten is vast nog wat onduidelijk. Eigenlijk ben je een carspotter als je foto’s 
+        trekt van auto’s die je tegenkomt. Je hebt natuurlijk zelf ook al gemerkt dat je deze exclusieve auto’s niet elke dag tegenkomt, afhankelijk van 
+        waar je woont. Dit is dan ook de reden waarom er ‘supercarevents’ worden georganiseerd. Er hangt altijd een leuke sfeer omdat de mensen die er 
+        rondlopen dezelfde hobby en je kan er enorm veel content bekomen die je later kan delen via sociale media, 
+        Wat ik persoonlijk één van de leukste dingen vindt. 
+      </p>
+    </div>
+    
+    <div id="footer">
+      <p>
+        KAREL DECOENE | 0478376274 | supercarsfromeverywhere@gmail.com
+      </p>
+    </div>
+  </div>
+</body>
+</html>
